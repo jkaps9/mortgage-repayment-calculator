8e760b0a7c78b2c61b965bfe2e48ddcc35018867
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-27 22:50:20 -0400

    remove default content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import "./styles.css";
-
-const mobileMenu = document.querySelector(".mobile-menu");
-const hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden");
-  hamburger.classList.toggle("close");
-});
