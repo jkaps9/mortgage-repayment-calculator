7c775ae963a8523ceb52eca9d9a5b536367096e9
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-29 00:11:45 -0400

    add functionality to calculate button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./styles.css";
 const clearButton = document.querySelector(".title-row > button");
 const inputs = document.querySelectorAll("input");
 
+const calculateButton = document.querySelector("#calculate");
+const emptyResults = document.querySelector(".empty-results");
+const completedResults = document.querySelector(".completed-results");
+
 clearButton.addEventListener("click", () => {
   inputs.forEach((input) => {
     if (input.type === "radio") {
@@ -11,4 +15,14 @@ clearButton.addEventListener("click", () => {
       input.value = "";
     }
   });
+
+  emptyResults.classList.remove("hidden");
+  completedResults.classList.add("hidden");
+});
+
+calculateButton.addEventListener("click", () => {
+  if (!emptyResults.classList.contains("hidden")) {
+    emptyResults.classList.add("hidden");
+    completedResults.classList.remove("hidden");
+  }
 });
