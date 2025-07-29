85303ccd6a92ce76b17d13694d147e09761aca73
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-29 00:23:02 -0400

    add function to calculate monthly payments

--- src/index.js
+++ src/index.js
@@ -26,3 +26,20 @@ calculateButton.addEventListener("click", () => {
     completedResults.classList.remove("hidden");
   }
 });
+
+function payment(principal, term, rate) {
+  // // M  = P * (r (1 + r)n) / ((1 + r)n - 1)
+  // M	Total monthly mortgage payment
+  // P	Principal loan amount
+  // r	Monthly interest rate: Lenders provide you an annual rate so you’ll need to divide that figure by 12 (the number of months in a year) to get the monthly rate. If your interest rate is 5 percent, your monthly rate would be 0.004167 (0.05/12=0.004167).
+  // n	Number of payments over the loan’s lifetime: Multiply the number of years in your loan term by 12 (the number of months in a year) to get the number of payments for your loan. For example, a 30-year fixed mortgage would have 360 payments (30x12=360).
+
+  const monthlyRate = rate / 12;
+  const months = term * 12;
+  const numerator = monthlyRate * Math.pow(1 + monthlyRate, months);
+  const denominator = Math.pow(1 + monthlyRate, months) - 1;
+  const monthlyPayment = principal * (numerator / denominator);
+  return Math.round((monthlyPayment + Number.EPSILON) * 100) / 100;
+}
+
+console.log(payment(300000, 30, 0.0525));
